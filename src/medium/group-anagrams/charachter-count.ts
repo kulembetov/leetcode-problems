972bea22e2dc4736203191dbf86f5e8ffc09e751
Projects <artur_kulembetov@outlook.com>
2024-03-05 13:12:34 +0700

    group-anagrams

---
+++ src/medium/group-anagrams/charachter-count.ts
@@ -0,0 +1,35 @@
+/* 
+time complexity: O(n*k), where n is the number of strings and k is the maximum length of a string,
+this complexity arises because we iterate over each character of each string to count character frequencies, 
+which is linear with respect to the string length, and we do this for each string in the input array
+
+space complexity: O(n*k), because the extra space is used for the hash table and the count arrays
+*/
+
+const groupAnagramsCharCount = (strs: string[]): string[][] => {
+  // initialize an object to act as a map for grouping anagrams
+  const map: { [key: string]: string[] } = {};
+
+  // iterate through each string in the input array
+  for (const str of strs) {
+    // initialize a count array to store the frequency of each letter
+    const count = new Array(26).fill(0);
+    for (const char of str) {
+      // increment the count for the current character
+      count[char.charCodeAt(0) - "a".charCodeAt(0)]++;
+    }
+
+    // create a key from the count array by joining its elements with a separator
+    const key = count.join("#");
+
+    // if the key doesn't exist in the map, initialize the value as an empty array
+    if (!map[key]) {
+      map[key] = [];
+    }
+    // add the current string to the array associated with the key in the map
+    map[key].push(str);
+  }
+
+  // return the values of the map as an array of arrays
+  return Object.values(map);
+};
---
+++ src/medium/group-anagrams/map.ts
@@ -0,0 +1,30 @@
+/*
+time complexity: O(n*k*log(k)) - where n is the number of strings in the input array,
+and k is the maximum length of a string in the array, this is because we sort each string individually
+which takes O(k*log(k)) time and we do this for each of the n strings
+
+space complexity: O(n*k) - because the extra space required depends on the number of items stored
+in the hash table, which stores at most n elements, and the space required to store sorted strings,
+which in the worst case requires k space per string
+*/
+
+const groupAnagramsMap = (strs: string[]): string[][] => {
+  // initialize a map to store groups of anagrams
+  const map = new Map<string, string[]>();
+
+  // iterate through each string in the input array
+  for (const str of strs) {
+    // sort the characters of the string to form a key
+    const key = str.split("").sort().join("");
+
+    if (map.has(key)) {
+      // if the key exists, append the current string to its group
+      map.get(key)!.push(str);
+    } else {
+      // otherwise, create a new group with the current string
+      map.set(key, [str]);
+    }
+  }
+  // convert the map values to an array of arrays and return
+  return Array.from(map.values());
+};
