ebd6d324c1e0b31e2ac87629fd854383f23db94e
Projects <artur_kulembetov@outlook.com>
2024-03-05 13:12:13 +0700

    two-sum

--- src/easy/two-sum/brute-force.ts
+++ src/easy/two-sum/brute-force.ts
@@ -1,5 +1,5 @@
-// Time Complexity: O(n^2) - because we're using two nested loops.
-// Space Complexity: O(1) - because we're not using any extra data structures that grow with input size.
+// time complexity: O(n^2) - because we're using two nested loops.
+// space complexity: O(1) - because we're not using any extra data structures that grow with input size.
 
 const twoSumBruteForce = (nums: number[], target: number): number[] => {
   for (let i = 0; i < nums.length; i++) {
--- src/easy/two-sum/two-pointers.ts
+++ src/easy/two-sum/two-pointers.ts
@@ -1,5 +1,5 @@
-// Time Complexity: O(n log n) - mainly due to the sort operation. The two-pointer traversal itself is O(n), but sorting takes precedence in complexity.
-// Space Complexity: O(1) or O(n) - depending on the sorting implementation, it might require O(1) (in-place sort) or O(n) (not in-place) extra space.
+// time complexity: O(n log n) - mainly due to the sort operation. The two-pointer traversal itself is O(n), but sorting takes precedence in complexity.
+// space complexity: O(1) or O(n) - depending on the sorting implementation, it might require O(1) (in-place sort) or O(n) (not in-place) extra space.
 
 const twoSumTwoPointers = (nums: number[], target: number): number[] => {
   nums.sort((a, b) => a - b);
