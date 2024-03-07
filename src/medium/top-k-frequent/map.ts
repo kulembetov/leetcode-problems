7b17caf90c88d32e429e15627b28c806d8fd67e1
Projects <artur_kulembetov@outlook.com>
2024-03-07 10:45:03 +0700

    top-k-frequent

--- src/medium/top-k-frequent/map.ts
+++ src/medium/top-k-frequent/map.ts
@@ -1,3 +1,15 @@
+/* 
+time complexity: O(n + n log n) - where n is the number of elements in the input array
+first, we iterate over the array to build the frequency map, which takes O(n) then, converting the map
+to an array and sorting it takes O(n log n) because we sort based on frequency
+the final operations are linear, so the dominant term is O(n log n)
+
+space complexity: O(n) - because the extra space required is primarily for the frequency map and
+the array converted from the map the frequency map will store at most n elements if all elements
+are unique, and converting this map to an array for sorting also requires space proportional to n
+*/
+
+
 const topKFrequent = (nums: number[], k: number): number[] => {
   const frequencyMap = new Map<number, number>();
 
@@ -10,7 +22,3 @@ const topKFrequent = (nums: number[], k: number): number[] => {
     .slice(0, k)
     .map(([num]) => num);
 };
-
-const nums = [1,1,1,6,7,7];
-
-topKFrequent(nums, 2);
