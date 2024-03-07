aea756691efd1b6061fd6253ba703327b186b8dd
Projects <artur_kulembetov@outlook.com>
2024-03-07 12:11:04 +0700

    product-except-self

---
+++ src/medium/product-except-self/array.ts
@@ -0,0 +1,21 @@
+// time complexity: O(n) - because we iterate through the array twice independently, not nested
+// space complexity: O(1) - excluding the output array, we only use a fixed amount of extra space
+
+const productExceptSelf = (nums: number[]): number[] => {
+  let answer = new Array(nums.length).fill(1);
+  
+  // calculate left products for each element
+  let leftProduct = 1;
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = leftProduct;
+    leftProduct *= nums[i];
+  }
+
+  // calculate right products and multiply with left products
+  let rightProduct = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answer[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+  return answer;
+};
