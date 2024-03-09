97f6b62eeb5cb0fab05b8ad24dbe948f2823b8c9
Projects <artur_kulembetov@outlook.com>
2024-03-09 16:49:25 +0700

    duplicates

--- src/easy/duplicates/set.ts
+++ src/easy/duplicates/set.ts
@@ -2,6 +2,5 @@
 // space Complexity: O(n) because we're storing the elements in a set
 
 const findDuplicatesSet = (nums: number[]): boolean => {
-  const hasDuplicates = new Set(nums).size !== nums.length;
-  return hasDuplicates;
+  return new Set(nums).size !== nums.length;
 };
