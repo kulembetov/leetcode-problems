9ba98b35b7dc00e67d5bf40a5aacf0f928dfa29b
Projects <artur_kulembetov@outlook.com>
2024-03-11 17:35:38 +0700

    single-value

--- src/easy/single-value/xor.ts
+++ src/easy/single-value/xor.ts
@@ -1,7 +1,7 @@
 // time complexity: O(n), because we iterate over each element once
 // space complexity: O(1), because the space used doesn't grow with the input size
 
-const singleNumberXor = (nums: number[]): number => {
+export const singleNumberXor = (nums: number[]): number => {
   let single = 0;
   for (let num of nums) {
     single ^= num;
