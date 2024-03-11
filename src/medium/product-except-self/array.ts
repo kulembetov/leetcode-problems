ce17915c1cc51d1a2fa0550098fb95e61e4775df
Projects <artur_kulembetov@outlook.com>
2024-03-11 17:33:43 +0700

    product-except-self

--- src/medium/product-except-self/array.ts
+++ src/medium/product-except-self/array.ts
@@ -3,7 +3,7 @@
 
 const productExceptSelf = (nums: number[]): number[] => {
   let answer = new Array(nums.length).fill(1);
-  
+
   // calculate left products for each element
   let leftProduct = 1;
   for (let i = 0; i < nums.length; i++) {
