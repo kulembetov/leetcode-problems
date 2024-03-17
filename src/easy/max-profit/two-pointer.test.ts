c2b128fbc1e3bba72f961f6f41e5bc0589e9cfd8
Projects <artur_kulembetov@outlook.com>
2024-03-17 14:48:12 +0700

    feat: add tests to easy/max-profit

---
+++ src/easy/max-profit/two-pointer.test.ts
@@ -0,0 +1,8 @@
+import maxProfitPointer from './two-pointer'
+
+describe("maxProfitPointer", () => {
+  it("should return 0 if there is no transactions", () => {
+    expect(maxProfitPointer([7, 1, 5, 3, 6, 4])).toBe(5);
+    expect(maxProfitPointer([7, 6, 4, 3, 1])).toBe(0);
+  });
+});
