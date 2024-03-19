f0c5bae7018f595abc3df8b756d37a59cf7a35fd
Projects <artur_kulembetov@outlook.com>
2024-03-19 13:31:32 +0700

    reverse-string

--- src/easy/reverse-string/two-pointer.ts
+++ src/easy/reverse-string/two-pointer.ts
@@ -1,7 +1,7 @@
 // time complexity: O(n), because of iterating over the string array once
 // space complexity: O(1), due to the size of the left and right variables is not increasing to the input array size
 
-const reverseString(s: []): void => {
+export const reverseString = (s: string[]): void => {
 	let left = 0;
 	let right = s.length - 1;
 
