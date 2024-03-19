f77688f3e557c90c7843338fe1bfad875ea33266
Projects <artur_kulembetov@outlook.com>
2024-03-19 14:12:51 +0700

    feat: add tests to /easy/palindrome

---
+++ src/easy/palindrome/two-pointers.test.ts
@@ -0,0 +1,28 @@
+import { isPalindrome } from "./two-pointers";
+
+describe("isPalindrome", () => {
+  it("should handle a palindrome", () => {
+    const s: string = "mom";
+    expect(isPalindrome(s)).toBe(true);
+  });
+
+  it("should handle an empty string", () => {
+    const s: string = "";
+    expect(isPalindrome(s)).toBe(true);
+  });
+
+  it("should reject a non palindrome", () => {
+    const s: string = "cat";
+    expect(isPalindrome(s)).toBe(false);
+  });
+
+  it("should process a numeric palindrome", () => {
+    const s: string = "12321";
+    expect(isPalindrome(s)).toBe(true);
+  });
+
+  it("should handle a complex palindrome", () => {
+    const s: string = "A man, a plan, a canal: Panama";
+    expect(isPalindrome(s)).toBe(true);
+  });
+});
--- src/easy/palindrome/two-pointers.ts
+++ src/easy/palindrome/two-pointers.ts
@@ -1,7 +1,7 @@
 // time complexity: O(n), due to the iterating the initial string once and over at most half of the normalized string
 // space complexity: O(1), the amount of any extra doesn't scale, variables size doesn't depend on the input size
 
-const isPalindrome = (s: string): boolean => {
+export const isPalindrome = (s: string): boolean => {
   const str = s.replace(/[\W_]+/g, "").toLowerCase();
 
   let left = 0;
