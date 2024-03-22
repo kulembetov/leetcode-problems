b8b99da40cf5026c736aee137bc099952f7eb560
Projects <artur_kulembetov@outlook.com>
2024-03-22 12:45:09 +0700

    feat: add tests to /easy/sock-merchant

---
+++ src/easy/sock-merchant/map.test.ts
@@ -0,0 +1,17 @@
+import { sockMerchant } from "./map";
+
+describe("sockMerchant", () => {
+  it("should return how many pairs of socks with matching colors they are", () => {
+    const arr = [1, 1, 2, 3, 3];
+    expect(sockMerchant(arr)).toBe(2);
+  });
+
+  it("should return zero pairs of socks for an empty array", () => {
+    expect(sockMerchant([])).toBe(0);
+  });
+
+  it("should return how many pairs of socks with matching colors they are for a large array", () => {
+    const arr = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9, 10, 4];
+    expect(sockMerchant(arr)).toBe(7);
+  });
+});
