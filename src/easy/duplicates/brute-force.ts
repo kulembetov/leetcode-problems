5ce31fb331c502a06479c258d0cd5ef33c4220df
Projects <artur_kulembetov@outlook.com>
2024-03-24 11:51:40 +0700

    duplicates

--- src/easy/duplicates/brute-force.ts
+++ src/easy/duplicates/brute-force.ts
@@ -1,5 +1,5 @@
-// time Complexity: O(n^2), iterate through the array with nested loops
-// space Complexity: O(1), no additional space needed
+// time complexity: O(n^2), iterate through the array with nested loops
+// space complexity: O(1), no additional space needed
 
 const findDuplicatesBruteForce = (nums: number[]): boolean => {
   for (let i = 0; i < nums.length; i++) {
