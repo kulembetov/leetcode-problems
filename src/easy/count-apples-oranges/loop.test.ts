cbc1bc381076afd0e17d2d8eeb09b909cd1a01f8
Projects <artur_kulembetov@outlook.com>
2024-04-18 16:50:51 +0700

    format: count-apples-oranges

--- src/easy/count-apples-oranges/loop.test.ts
+++ src/easy/count-apples-oranges/loop.test.ts
@@ -3,7 +3,7 @@ import { countApplesAndOranges } from './loop';
 describe('countApplesAndOranges', () => {
   it('should count the number of apples and oranges that land on the house', () => {
     const consoleSpy = jest.spyOn(console, 'log');
-    
+
     countApplesAndOranges(5, 15, 7, 11, [-2, 2, 1], [5, -6]);
 
     expect(consoleSpy).toHaveBeenNthCalledWith(1, 1);
