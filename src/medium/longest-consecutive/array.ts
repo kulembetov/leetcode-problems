42cfda41268a78fe0ef87bf9feb68ec1de66137f
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-10-06 15:58:34 +0700

    chore: come up with a new solution for the /medium/longest-consecutive

--- src/medium/longest-consecutive/array.ts
+++ src/medium/longest-consecutive/array.ts
@@ -2,30 +2,21 @@
 // space complexity: O(1) or O(n) depending on the sorting algorithm implementation
 
 const longestConsecutiveArray = (nums: number[]): number => {
-  // edge case: if array is empty, return 0
   if (nums.length === 0) return 0;
 
-  // sort the array to bring consecutive elements together
-  const sortedArray: number[] = nums.sort(
-    (a: number, b: number): number => a - b
-  );
-  // to store the length of the longest sequence found
-  let max: number = 0;
-  // to store the length of the current sequence
-  let currentMax: number = 1;
+  const sortedArr = nums.sort((a, b) => a - b);
 
-  // iterate through the sorted array to find consecutive sequences
-  for (let i = 0; i < sortedArray.length - 1; i++) {
-    // if current element and next element form a consecutive sequence, increment currentMax
-    if (sortedArray[i] + 1 === sortedArray[i + 1]) {
-      currentMax++;
-      // if current element and next element are not consecutive and not equal, update max and reset currentMax
-    } else if (sortedArray[i] !== sortedArray[i + 1]) {
-      max = Math.max(max, currentMax);
-      currentMax = 1;
+  let maxLength = 0;
+  let currentLength = 1;
+
+  for (let i = 1; i < sortedArr.length; i++) {
+    if (sortedArr[i] === sortedArr[i - 1] + 1) {
+      currentLength++;
+    } else if (sortedArr[i] !== sortedArr[i - 1]) {
+      maxLength = Math.max(maxLength, currentLength);
+      currentLength = 1;
     }
   }
 
-  // return the maximum of max and currentMax to handle the case where the longest sequence ends at the last element
-  return Math.max(max, currentMax);
+  return Math.max(maxLength, currentLength);
 };
