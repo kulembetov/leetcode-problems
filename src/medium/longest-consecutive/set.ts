965465666fdaa17a20372e3c95bc013e8fa4fecd
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-10-06 18:04:01 +0700

    chore: come up with a new solution for the /medium/longest-consecutive

--- src/medium/longest-consecutive/set.ts
+++ src/medium/longest-consecutive/set.ts
@@ -2,29 +2,20 @@
 // space complexity: O(n) due to the additional storage used by the set
 
 const longestConsecutiveSet = (nums: number[]): number => {
-  // create a set from the array to eliminate duplicates and allow for O(1) lookups
   const numSet: Set<number> = new Set(nums);
-  // to store the length of the longest sequence found
-  let longestSreak: number = 0;
+  let longest: number = 0;
 
-  // iterate through the numbers
-  for (const num of nums) {
-    // if the previous number is not in the set, this could be the start of a sequence
-    if (!numSet.has(num - 1)) {
-      // start of the current sequence
-      let currentNum: number = num;
-      // length of the current sequence
-      let currentStreak: number = 1;
+  for (let n of nums) {
+    if (!numSet.has(n - 1)) {
+      let length = 0;
 
-      // while the next consecutive number is in the set, increment currentNum and currentStreak
-      while (numSet.has(currentNum + 1)) {
-        currentNum += 1;
-        currentStreak += 1;
+      while (numSet.has(n + length)) {
+        length += 1;
       }
-      // update the longest streak found so far
-      longestSreak = Math.max(longestSreak, currentStreak);
+
+      longest = Math.max(length, longest);
     }
   }
-  // return the longest streak
-  return longestSreak;
+
+  return longest;
 };
